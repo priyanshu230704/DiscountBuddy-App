c9837333986efc1762b91eda0532c6be9cbf0d19
Priyanshu Chavda <priyanshu.chavda@bluexkye.com>
2026-04-13 10:40:56 +0530

    image size compressed and packages removed

--- lib/design/app_avatars.dart
+++ lib/design/app_avatars.dart
@@ -1,10 +1,10 @@
 class AppAvatars {
   static const List<String> presetAvatars = [
-    'assets/png/file_00000000c3e871f8962abd24bfb6a3d6.png',
-    'assets/png/file_00000000cacc71fd865d676a71adc7d3.png',
-    'assets/png/file_000000001e6871fda54748ab79191542.png',
-    'assets/png/file_000000007f4471f8bbbb2883c1431131.png',
-    'assets/png/file_0000000087bc71fd95e1f5286cf89746.png',
-    'assets/png/file_00000000747071fdb574dc79c203af48.png',
+    'assets/png/profile1.png',
+    'assets/png/profile2.png',
+    'assets/png/profile3.png',
+    'assets/png/profile4.png',
+    'assets/png/profile5.png',
+    'assets/png/profile6.png',
   ];
 }
